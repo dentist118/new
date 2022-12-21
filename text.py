049bf115799111e8cfe2d1c1510270899787ca39
Dr.Mahmoud <82735653+dentist118@users.noreply.github.com>
2022-12-21 23:59:36 +0200

    Update text.py

--- text.py
+++ text.py
@@ -1,4 +1,21 @@
 import streamlit as st 
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('awesome-gif.gif')    
+
 st.title("A Beautiful Title")
 st.header("Article header")
 st.subheader("Good looking sub-header")
